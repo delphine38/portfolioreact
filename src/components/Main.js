b50559bb488f4626a37b6fcc4ed59ee80229444a
delphine <delphin38@live.fr>
2022-04-28 12:16:54 +0200

    avancement realisation main

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,11 @@ import Figma from '../images/figma.png';
 import Vscode from '../images/vscode.png';
 import WebStorm from '../images/webStorm.png';
 import Mysql from '../images/mySql.png';
+import Mathsjs from '../images/mathsjs.png';
+import Iconefleche from '../images/iconefleche.png';
+import Compteurreact from '../images/compteurreact.png';
+import Argophp from '../images/argophp.jpg';
+import Stickers from '../images/stikers.jpg';
 
 const Main = () => {
     return(
@@ -53,6 +58,79 @@ const Main = () => {
                     <div class="row w-100 p-3 d-flex justify-content-center">
                         <h3 class="text-center pt-5 mb-5 pe-5 me-5" id="realisationcompetences">Mes réalisations</h3>
                     </div>
+
+                    <div class="container-fluid row  d-flex justify-content-center">
+
+                        <article class="col-sm-3 col-lg-3">
+                            <div class="justify-content-center h-100">
+                                <div class="image row justify-content-center  h-80 d-flex flex-wrap align-items-center mt-2">
+                                    <img src={Argophp} alt="Argonautes en php" />
+                                </div>
+                                <div class="text-center mt-2">
+                                    <section class="fleche">
+                                        
+                                    </section>
+                                    <button class="bcompetences">Php et PhpMyAdmin</button>
+                                </div>
+                                    <h5>Ici argo php bientôt</h5>
+                            </div>
+                        </article>
+
+                        <article class="col-sm-3 col-lg-3">
+                            <div class="justify-content-center h-100">
+                                <div class="image row justify-content-center  h-80 d-flex flex-wrap align-items-center mt-2"></div>
+                                    <img src={Stickers} alt="Stickers en php" />
+                                </div>
+                                <div class="text-center mt-2">
+                                    <section class="fleche">
+                                        
+                                    </section>
+                                    <button class="bcompetences">Php et PhpMyAdmin</button>
+                                </div>
+                                    <h1>Stickers php bientot</h1>
+                        </article>
+
+                        <article class="col-sm-3 col-lg-3">
+                            <div class="justify-content-center h-100">
+                                <div class="image row justify-content-center  h-80 d-flex flex-wrap align-items-center mt-2">
+                                    <a href={"https://delphine38.github.io/testdecalculjavascript"} target="_blank"><img src={Mathsjs} alt="revision de maths" /></a>
+                                </div>
+                                <div class="text-center mt-2">
+                                    <section class="fleche">
+                                        <a href={"https://delphine38.github.io/testdecalculjavascript"} target="_blank"><img src={Iconefleche} alt="image de fleche" /></a>
+                                        
+                                        {/* il y aura image fleche et lien */}
+                                    </section>
+                                        <button class="bcompetences">html</button>
+                                        <button class="bcompetences">Javascript</button>
+                                        <button class="bcompetences">Lien du github</button>
+                                </div>
+                                <h5 class="text-left">Application pour réviser ses Maths</h5>
+                            </div>
+                        </article>
+
+                        <article class="col-sm-3 col-lg-3">
+                            <div class="justify-content-center h-100">
+                            <div class="image row justify-content-center  h-80 d-flex flex-wrap align-items-center mt-2">
+                                <a href="#" target="_blank"><img src={Compteurreact} alt="compteur en react" />
+                                </a>
+
+                                <a href={"https://delphine38.github.io/compteurreact/"} target="_blank"><img src={Iconefleche}
+                                    alt="compteur en react" />
+                                </a>
+                            
+                            </div>
+                            <div class="text-center mt-2">
+                                <button class="bcompetences">react</button>
+                            </div>
+                            <h5 class="text-left">Application en react pour ajouter ou enlever</h5>
+                            </div>
+                        </article>
+
+
+
+                    </div>
+
                 </div>
 
             </section>
